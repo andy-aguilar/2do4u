1ae628071fb60a1a776c70bbc7de715159b22f21
andy-aguilar <aaguil3@gmail.com>
2021-04-06 13:29:09 -0500

    Start checklist in README

--- src/components/form.js
+++ src/components/form.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
 
 export default class Form extends Component {
+
+    state = {
+        todo: ""
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            todo: e.target.value
+        })
+    }
+
+    handleSubmit = () => {
+
+    }
+
     render() {
         return (
             <div>
                 <h1>Add Todo:</h1>
             <form className="form">
-                <input type="text" />
+                <input type="text" onChange={this.handleChange} />
                 <input type="submit" value="Add Item"/>
             </form>
             </div>
